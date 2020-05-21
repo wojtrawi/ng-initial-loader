9e7a2da70e3b587334d92a11ea18aa8ed1b29717
wojtrawi <wojtrawi@gmail.com>
2020-05-21 17:05:29 +0200

    Add moment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  moment = moment;
   title = 'ng-initial-loader';
 }
